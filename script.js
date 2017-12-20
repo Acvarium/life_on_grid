8c43157a8eed1e2fd5e8c0f59e579b859a29989e
Acvarium <vitshmorgun@gmail.com>
2017-12-21 01:03:01 +0200

    Let there be life

---
+++ script.js
@@ -0,0 +1,42 @@
+var size = 60;
+var table = document.getElementById("table");
+
+function getCell(x, y) {
+  return table.rows[y].children[x].classList;
+}
+
+function nextGen() {
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      getCell(x, y).toggle("corpse", getCell(x, y).contains('alive'));
+    }
+  }
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      let neighbors = countNeighbors(x, y);
+      getCell(x, y).toggle('alive', neighbors == 3 || (getCell(x, y).contains('corpse') && neighbors == 2));
+    }
+  }
+}
+
+function countNeighbors(x, y) {
+  let sum = 0;
+  for (let dy = -1; dy <= 1; dy++) {
+    for (let dx = -1; dx <= 1; dx++) {
+      if (dx || dy) {
+        let cell = getCell((x + dx + size) % size, (y + dy + size) % size);
+        sum += cell.contains('corpse');
+      }
+    }
+  }
+  return sum;
+}
+
+for (let y = 0; y < size; y++) {
+  let row = table.insertRow();
+  for (let x = 0; x < size; x++) {
+    row.insertCell().classList.toggle('alive', Math.random() < 0.5);
+  }
+}
+
+setInterval(nextGen, 100);
